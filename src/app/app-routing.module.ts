cfb7613808851f88b102d44f1cd7b8bee5cf881d
Brwninhouu <bru_o_gato@hotmail.com>
2021-08-27 11:06:00 -0300

    Aula Angular

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CelularComponent } from './celular/celular.component';
+import { ClienteComponent } from './cliente/cliente.component';
+import { HomeComponent } from './home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+
+
+{ path:'celular',component:CelularComponent },
+{ path:'cliente',component:ClienteComponent },
+{ path:'home',component:HomeComponent }
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { MenuComponent } from './menu/menu.component';
+import { CelularComponent } from './celular/celular.component';
+import { ClienteComponent } from './cliente/cliente.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    MenuComponent,
+    CelularComponent,
+    ClienteComponent
   ],
   imports: [
     BrowserModule,
